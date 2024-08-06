d5231ea6c35a30ccc242a9279fd865d7274dba43
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-08-06 14:33:26 +0100

    new push hello and fine

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,14 +3,16 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Screen1 from "../project/Screen1";
+import Screen2 from "../project/Screen2";
 
 
 const _layout = () => {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator options={{screenOption: false}}>
         Stack.Screen name="Screen1" component={Screen1} />
+        Stack.Screen name="Screen2" component={Screen2} />
       </Stack.Navigator>
     </NavigationContainer>
   );
