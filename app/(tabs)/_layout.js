629f1bf6500bd23e3df2f896fa151fd9f40fdc14
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-09-04 11:50:38 +0100

    sksks

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,14 +1,71 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React from 'react';
 
 const _layout = () => {
+
+  const data = [
+    {
+      id: 1,
+      title: 'Ade',
+      details: "Nice wears for guys",
+      // bg: 'green',
+    }, {
+      id: 2,
+      title: 'Thomas',
+      details: "Nice wears for guys",
+      // bg: "red"
+    }, {
+      id: 3,
+      title: 'John',
+      details: "Nice wears for guys",
+      // bg: "indigo"
+    },
+  ]
+
+  const userData = { name: "favour", class: "12" }
+
   return (
-    <View>
-      <Text>_layout</Text>
+    <View style={styles.page}>
+      <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Comparison</Text>
+
+      <View style={{ marginTop: 30 }}>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => {
+            return (
+              <View>
+                {item.id !== 2 &&
+                  <View style={[styles.container, { backgroundColor: item.id === 5 ? "green" : "black" }]}>
+                    <Text style={{ fontSize: 15, color: "white", fontWeight: "bold" }}>{item.title}</Text>
+                    <Text style={{ fontSize: 15, color: "white" }}>{item.details}</Text>
+                    <Text style={{ fontSize: 15, color: "white" }}>{item.king}</Text>
+                  </View>
+                }
+              </View>
+            )
+          }}
+        />
+      </View>
     </View>
   )
 }
 
 export default _layout
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingHorizontal: 10,
+    paddingTop: 5,
+  },
+  container: {
+    height: 70,
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 5,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    marginBottom: 20,
+  },
+});
