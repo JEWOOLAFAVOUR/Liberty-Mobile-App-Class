f925dc03eaf671a25e4e7a23a06e8d738190a3eb
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-09-10 14:34:46 +0100

    sksksk

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,39 +1,56 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native'
 import React from 'react'
 
 const _layout = () => {
 
-  const friends = ['Ade', "Bola", "Bimbo"]
+  const profileData = [
+    {
+      id: 1,
+      title: "Personal Data",
+      iconName: require("../../assets/icons/person.png"),
+    }, {
+      id: 2,
+      title: "Settings",
+      iconName: require("../../assets/icons/person.png"),
+    }, {
+      id: 3,
+      title: "Settings",
+      iconName: require("../../assets/icons/person.png"),
+    },
+    {
+      id: 4,
+      title: "Settings",
+      iconName: require("../../assets/icons/person.png"),
+    },
+  ]
 
-  const x = 10 // normal variable
-
-  const y = {
-    name: "x",
-    level: '100'
-  } // Object
-
-  const z = [1, 2, 3, 4] // array
 
   return (
     <View style={styles.page}>
-      <Text style={{ fontSize: 30, color: 'black', fontWeight: 'bold', marginBottom: 30 }}>List Class</Text>
+      {/* HEADER */}
+      <View style={{ flexDirection: "row", alignItems: 'center' }}>
+        <Image source={require('../../assets/images/man.jpg')} style={{ height: 60, width: 60, borderRadius: 10 }} />
+        <View style={{ marginLeft: 14 }}>
+          <Text style={{ fontSize: 17, color: "#000", fontWeight: "bold" }}>Jewoola Favour</Text>
+          <Text style={{ fontSize: 14, color: "#000" }}>Mobile App Developer</Text>
+        </View>
+      </View>
 
-
-      <FlatList
-        data={z}
-        numColumns={2}
-        renderItem={({ item }) => {
-          return (
-            <View style={styles.container}>
-              <Text style={{ fontSize: 20, color: 'black', fontWeight: 'bold' }}>{item}</Text>
-            </View>
-          )
-        }}
-
-
-
-
-      />
+      {/* LIST  */}
+      <View style={{ marginTop: 40 }}>
+        <FlatList
+          data={profileData}
+          renderItem={({ item }) => {
+            return (
+              <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 30, }}>
+                <Image source={item.iconName} style={{ height: 24, width: 24 }} />
+                <Text style={{ marginLeft: 15, flex: 1 }}>{item.title}</Text>
+                <Image source={require('../../assets/icons/arrowright.png')} style={{ height: 15, width: 15 }} />
+              </View>
+            )
+          }}
+        />
+      </View>
 
 
     </View>
@@ -46,103 +63,7 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
     backgroundColor: '#ffffff',
-    paddingHorizontal: 20,
-    paddingTop: 20,
-  },
-  container: {
-    height: 50,
-    width: 150,
-    borderWidth: 1,
-    borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 20,
+    paddingHorizontal: 15,
+    paddingTop: 15,
   },
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { StyleSheet, Text, View, FlatList } from 'react-native';
-// import React from 'react';
-
-// const _layout = () => {
-
-//   const data = [
-//     {
-//       id: 1,
-//       title: 'Ade',
-//       details: "Nice wears for guys",
-//       // bg: 'green',
-//     }, {
-//       id: 2,
-//       title: 'Thomas',
-//       details: "Nice wears for guys",
-//       // bg: "red"
-//     }, {
-//       id: 3,
-//       title: 'John',
-//       details: "Nice wears for guys",
-//       // bg: "indigo"
-//     },
-//   ]
-
-//   const userData = { name: "favour", class: "12" }
-
-//   return (
-//     <View style={styles.page}>
-//       <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Comparison</Text>
-
-//       <View style={{ marginTop: 30 }}>
-//         <FlatList
-//           data={data}
-//           renderItem={({ item }) => {
-//             return (
-//               <View>
-//                 {item.id !== 2 &&
-//                   <View style={[styles.container, { backgroundColor: item.id === 5 ? "green" : "black" }]}>
-//                     <Text style={{ fontSize: 15, color: "white", fontWeight: "bold" }}>{item.title}</Text>
-//                     <Text style={{ fontSize: 15, color: "white" }}>{item.details}</Text>
-//                     <Text style={{ fontSize: 15, color: "white" }}>{item.king}</Text>
-//                   </View>
-//                 }
-//               </View>
-//             )
-//           }}
-//         />
-//       </View>
-//     </View>
-//   )
-// }
-
-// export default _layout
-
-// const styles = StyleSheet.create({
-//   page: {
-//     flex: 1,
-//     backgroundColor: "white",
-//     paddingHorizontal: 10,
-//     paddingTop: 5,
-//   },
-//   container: {
-//     height: 70,
-//     borderWidth: 1,
-//     borderColor: "gray",
-//     borderRadius: 5,
-//     justifyContent: 'center',
-//     paddingHorizontal: 20,
-//     marginBottom: 20,
-//   },
-// });
